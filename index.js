01bbf544a3c8b9164ecb0b6c9fec8e2dcb375da0
Lyubov <zenkova.lyuba@gmail.com>
2022-12-13 10:07:26 +0300

    homework3

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-// const colors = require('colors');
-import colors from 'colors';
+import fs from 'fs'
+import readline from 'readline'
 
-const interval = process.argv.slice(2);
-const number1 = Number(parseInt(interval[0], 10));
-const number2 = Number(parseInt(interval[1], 10));
-const simpleList = [];
-if((number1 > number2) || (isNaN(number1)) || (isNaN(number2))) {
-    console.log(colors.red("Неверный интервал"));
-    
-}
-if((number2 < 2) && (number1 < number2)) {
-    console.log(colors.red('В диапазоне отсутствуют простые числа'));
-}
-if((number2 >= 2) && (!isNaN(number1)) && (number1 < number2)) {
-    nextPrime:
-    for(let i = 2; i <= number2; i++) {
-        for(let j = 2; j < i; j++) {
-            if(i%j === 0) continue nextPrime; 
-        }
-        simpleList.push(i); 
-    }
-    let q=0;
-    for(let k = 0; k < simpleList.length; k++) {
-        if(q === 0){
-            console.log(colors.red(simpleList[k]));
-        }
-        if(q === 1){
-            console.log(colors.yellow(simpleList[k]));
-        }
-        if(q === 2){
-            console.log(colors.green(simpleList[k]));
-        }
-        q++;
-        if(q===3) q=0;
-       
-    }
-}
+const ip1 = '89.123.1.41'
+const ip2 = '34.48.240.111'
 
+const readInterface = readline.createInterface({
+  input: fs.createReadStream('./access_tmp.log'),
+})
 
+readInterface.on('line', function (line) {
+  if (line.includes(ip1)) {
+    const writeStream = fs.createWriteStream(`./${ip1}_requests.log`, {
+      flags: 'a',
+      encoding: 'utf8',
+    })
+    writeStream.write(line)
+    writeStream.write('\n')
+  }
+  if (line.includes(ip2)) {
+    const writeStream = fs.createWriteStream(`./${ip2}_requests.log`, {
+      flags: 'a',
+      encoding: 'utf8',
+    })
+    writeStream.write(line)
+    writeStream.write('\n')
+  }
+})
