f0ae9c5d2a96f0afae6bd175a0f1b9d8c1362670
Lyubov <zenkova.lyuba@gmail.com>
2022-12-16 19:20:50 +0300

    hw4

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 import fs from 'fs'
+import inquirer from 'inquirer'
+import path from 'path'
 import readline from 'readline'
 
-const ip1 = '89.123.1.41'
-const ip2 = '34.48.240.111'
+const rl = readline.createInterface(process.stdin, process.stdout)
+rl.question('Укажите каталог: ', (dirname) => {
+  const dir = dirname ? dirname : process.cwd()
+  const files = fs.readdirSync(dir)
 
-const readInterface = readline.createInterface({
-  input: fs.createReadStream('./access_tmp.log'),
+  requirerPrompt(dir, files)
 })
 
-readInterface.on('line', function (line) {
-  if (line.includes(ip1)) {
-    const writeStream = fs.createWriteStream(`./${ip1}_requests.log`, {
-      flags: 'a',
-      encoding: 'utf8',
-    })
-    writeStream.write(line)
-    writeStream.write('\n')
+const contentIncluds = (fileContent, strname) => {
+  if (fileContent.includes(strname)) {
+    console.log('Строка есть в файле')
+  } else {
+    console.log('Строки нет в файле')
   }
-  if (line.includes(ip2)) {
-    const writeStream = fs.createWriteStream(`./${ip2}_requests.log`, {
-      flags: 'a',
-      encoding: 'utf8',
+}
+
+const requirerPrompt = (dir, files = []) => {
+  inquirer
+    .prompt([
+      {
+        name: 'filepath',
+        type: 'list',
+        message: 'Выберите файл: ',
+        choices: files,
+      },
+    ])
+    .then((answer) => {
+      if (fs.lstatSync(path.join(dir, answer.filepath)).isFile()) {
+        const fileContent = fs
+          .readFileSync(path.join(dir, answer.filepath))
+          .toString()
+        console.log(fileContent)
+        rl.close()
+
+        if (fileContent.length) {
+          const rl1 = readline.createInterface(process.stdin, process.stdout)
+          rl1.question('Укажите строку для поиска: ', (strname) => {
+            contentIncluds(fileContent, strname)
+            rl1.close()
+          })
+        } else {
+          console.log('Файл пуст')
+        }
+      } else if (fs.lstatSync(path.join(dir, answer.filepath)).isDirectory()) {
+        const dirContent = fs.readdirSync(path.join(dir, answer.filepath))
+
+        if (dirContent.length) {
+          requirerPrompt(path.join(dir, answer.filepath), dirContent)
+        } else {
+          console.log('Каталог пуст')
+        }
+      }
     })
-    writeStream.write(line)
-    writeStream.write('\n')
-  }
-})
+}
